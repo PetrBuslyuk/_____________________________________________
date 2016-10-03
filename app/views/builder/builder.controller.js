efdb98eb69bff6b8c62df3053a5aa9cff793e4ab
PetrBuslyuk <skyliner270594@mail.ru>
2016-10-03 09:28:57 +0300

    change

--- app/views/builder/builder.controller.js
+++ app/views/builder/builder.controller.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.controller('BuilderCtrl', function($scope, dragulaService){
+.controller('BuilderCtrl', function($scope, dragulaService, $sce){
 
     $scope.toolsSlide = true;
     dragulaService.options($scope, 'builder-pain', {
@@ -13,8 +13,8 @@ angular.module('app')
     });
 
     $scope.$on('builder-pain.drag', function (e, el) {
-      // console.log($scope.items);
-      // console.log(e, el);
+      console.log($scope.items);
+      console.log(e, el);
       // el.removeClass('ex-moved');
     });
     //
@@ -36,13 +36,13 @@ angular.module('app')
         "items":[{
             "name": "input",
             "description": "input",
-            "html": "<input style='width:100px; top:0px; left:0;' type='text'>",
+            "html": $sce.trustAsHtml("<input element-dragged style='width:100px;' type='text'>"),
             "styles": "width: 100px;",
             "class": ""
         },{
             "name": "button",
             "description": "button",
-            "html": "<button style='width:100px; top:0px; left:0;'>Btn</button>",
+            "html": $sce.trustAsHtml("<button element-dragged style='width:100px;'>Btn</button>"),
             "styles": "width: 100px;",
             "class": ""
         }],
@@ -72,36 +72,12 @@ angular.module('app')
 })
 .directive('elementDragged', ['$compile', function($compile){
   return {
-    restrict: 'E',
-    scope: {
-      element: '='
-    },
+    restrict: 'AE',
     link: function(scope, elm, attrs){
-      var startX, startY, initialMouseX, initialMouseY;
-      // console.log(scope.element)
-      var linkFn = $compile(scope.element.html);
-      var content = linkFn(scope);
-      elm.append(content);
-
-      elm.bind("dragstart", function($event) {
-        console.log('dragstarted')
-          startX = elm.prop('offsetLeft');
-          startY = elm.prop('offsetTop');
-          initialMouseX = $event.clientX;
-          initialMouseY = $event.clientY;
-          return false;
-      })
-
-      elm.bind("dragend", function($event) {
-          var dx = $event.clientX - initialMouseX;
-          var dy = $event.clientY - initialMouseY;
-          elm.css({
-              top:  startY + dy + 'px',
-              left: startX + dx + 'px'
-          });
-          console.log('dy',dy,'dx',dx);
-          return false;
-      })
+      console.log("here")
+      //$compile(scope.element.html)(scope);
+      // var content = linkFn(scope);
+      // elm.append(content);
     }
   }
 }])
